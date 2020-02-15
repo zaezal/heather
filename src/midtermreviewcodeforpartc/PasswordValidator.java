4735e4cf92e253017e7197254115f2e2e767e84c
Heather <Heather@10.216.18.162>
2020-02-15 13:25:21 -0500

    new class create

--- src/midtermreviewcodeforpartc/PasswordValidator.java
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -10,20 +10,25 @@ package midtermreviewcodeforpartc;
  * @author Heather
  */
 public class PasswordValidator {
-    int specialCharCount=0;
+    
+    public boolean validatePassword(String password){
+        boolean flag = false;
+   
+        int specialCharCount=0;
             //iterate over each character to see if it is a special character
-    // variable password , validpassword ? 
-    //illegal start of type? 
-            for(int i=0;i<password.length(); i++)
-            {
-                if(!(Character.isLetterOrDigit(password.charAt(i))))
-                {
+            
+        for(int i=0;i<password.length(); i++){
+            
+            if(!(Character.isLetterOrDigit(password.charAt(i)))){
+              
                     //now we know there is at least one special character
                     specialCharCount++;
                 }
             }
-            if(specialCharCount>0 &&password.length()>7)
-            {
-                validPassword=true;
+            if(specialCharCount>0 &&password.length()>7){
+                flag = true;
+            }
+           return flag;
             } 
 }
+
